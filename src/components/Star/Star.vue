0852eca25d925fce9f77403eac79f9cb0e36702e
mingchun2002 <mingchun2002@163.com>
2020-08-10 22:48:35 +0800

    init gshop

---
+++ src/components/Star/Star.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">star</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+    }
+  },
+  created () {},
+  mounted () {},
+  watch: {},
+  computed: {},
+  methods: {}
+}
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
